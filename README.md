f73564c7f4d30bb5a34d80bc538cec4029442cab
Victor Milovanov <lostfreeman@gmail.com>
2020-02-08 16:22:38 -0800

    library stub

---
+++ README.md
@@ -0,0 +1,1 @@
+.NET Standard 2.0 library for reading [RosBag format](https://wiki.ros.org/Bags/Format/2.0).
---
+++ src/RosBagReader.cs
@@ -0,0 +1,25 @@
+﻿namespace ROS {
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class RosBagReader: IDisposable {
+        readonly Stream source;
+
+        public RosBagReader(Stream source) {
+            this.source = source ?? throw new System.ArgumentNullException(nameof(source));
+        }
+
+        public async Task<bool> ReadAsync() {
+            throw new NotImplementedException();
+        }
+
+        public async Task SkipAsync() {
+            throw new NotImplementedException();
+        }
+
+        void Dispose() => this.source.Dispose();
+        void IDisposable.Dispose() => this.Dispose();
+        public void Close() => this.Dispose();
+    }
+}
