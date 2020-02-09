b2fd3e516105efb13b1718638f0e08d498b6b1b2
Victor Milovanov <lostfreeman@gmail.com>
2020-02-08 17:03:09 -0800

    added RosBagHeader

---
+++ src/RosBagHeader.cs
@@ -0,0 +1,19 @@
+﻿namespace ROS {
+    /// <summary>
+    /// The bag header record occurs once in the file as the first record.
+    /// </summary>
+    public struct RosBagHeader {
+        /// <summary>
+        /// Offset of first record after the chunk section
+        /// </summary>
+        public long FirstRecordOffset { get; }
+        /// <summary>
+        /// Number of unique connections in the file
+        /// </summary>
+        public int ConnectionCount { get; }
+        /// <summary>
+        /// Number of chunk records in the file
+        /// </summary>
+        public int ChunkCount { get; }
+    }
+}
