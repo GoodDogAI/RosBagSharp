317e9bdd75a1c0e20dbc4a55ad1e3b9e8ffc0866
Victor Milovanov <lostfreeman@gmail.com>
2020-02-08 16:37:44 -0800

    added RosBagRecordType

---
+++ src/RosBagRecordType.cs
@@ -0,0 +1,10 @@
+﻿namespace ROS {
+    public enum RosBagRecordType {
+        BagHeader = 0x03,
+        Chunk = 0x05,
+        Connection = 0x07,
+        Message = 0x02,
+        Index = 0x04,
+        ChunkInfo = 0x06,
+    }
+}
